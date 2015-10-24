563d1ce61052f4f6a4fa7a89cbcc6951dec1e549
Peter Lesslie <pclesslie@gmail.com>
2015-10-24 17:36:23 -0500

    Save work

--- leaded_cribs/main.cpp
+++ leaded_cribs/main.cpp
@@ -6,12 +6,9 @@ int main(int argc, char ** argv) {
     int n;
     std::cin >> n;
     for (int i = 0; i < n; ++i) {
-        std::string perm, word;
-        std::cin >> word;
-        perm = word;
+        std::string perm, word; std::cin >> word; perm = word;
         std::sort(perm.begin(), perm.end());
-        int count = 1;
-        for (;perm != word && std::next_permutation(perm.begin(), perm.end()); ++count);
+        int count = 1; for (;perm != word && std::next_permutation(perm.begin(), perm.end()); ++count);
         std::cout << count << std::endl;
     }
     return 0;
